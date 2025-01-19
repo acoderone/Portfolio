80e52ac7bdf2c4e52f31426ecdf04a7e6cffe010
Acoderone <avi5200786@gmail.com>
2025-01-19 17:42:51 +0530

    changes

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,16 +2,14 @@ import React from 'react';
 
 function Footer() {
   return (
-    <div className='rounded-lg p-2 '>
-    <div className='flex justify-center font-semibold '> Contact ME</div>
+    <div className='flex flex-col rounded-lg p-2 justify-center items-center '>
+    <div className='  font-medium '>Want to contact me?</div>
      
-      <div>
-        EmailID - <a href="https://mail.google.com/mail/?view=cm&fs=1&to=avi5200786@gmail.com&su=SUBJECT&body=BODY" 
-        target="_blank">avi5200786@gmail.com</a>
+      <div className='font-semibold p-2'>
+         <a href="https://mail.google.com/mail/?view=cm&fs=1&to=avi5200786@gmail.com&su=SUBJECT&body=BODY" 
+        target="_blank">CLICK HERE</a>
       </div>
-      <div>
-        Phone no. - 9453244355
-      </div>
+      
     </div>
   );
 }
